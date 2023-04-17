88b74ab1ec1cc12e43b40d1a6bbceb2368d8a5bf
nitigithub123 <130463009+nitigithub123@users.noreply.github.com>
2023-04-17 10:04:59 +0530

    Add files via upload

---
+++ 2ques6.py
@@ -0,0 +1,7 @@
+x = int(input("Enter side of triangle"))
+y = int(input("Enter side of triangle"))
+z = int(input("Enter side of triangle"))
+if x+y>z and y+z>x and z+x>y :
+    print("yes")
+else:
+    print ("no")
